727049e11d2e1538cb88de4cc64be5b0456c74ee
Matthew Salvatore <salvatore@railroad19.com>
2019-12-03 15:48:50 -0500

    Initial Commit

---
+++ src/test/java/com/msalvatore/graphqlplayground/GraphqlPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msalvatore.graphqlplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
