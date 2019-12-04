a3140e640e0d3cddc9334d1693f66ef86554f29c
Matthew Salvatore <salvatore@railroad19.com>
2019-12-04 15:07:38 -0500

    Create GraphQL vehicle schema and wire up mutations/queries to service

---
+++ src/main/java/com/msalvatore/graphqlplayground/mutation/VehicleMutation.java
@@ -0,0 +1,21 @@
+package com.msalvatore.graphqlplayground.mutation;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.msalvatore.graphqlplayground.entity.Vehicle;
+import com.msalvatore.graphqlplayground.service.VehicleService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VehicleMutation implements GraphQLMutationResolver {
+
+    private final VehicleService vehicleService;
+
+    public VehicleMutation(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    public Vehicle createVehicle(String type, String modelCode, String brandName, String launchDate) {
+        // TODO: Can we accept a LocalDate instead of a String?
+        return vehicleService.createVehicle(type, modelCode, brandName, launchDate);
+    }
+}
---
+++ src/main/java/com/msalvatore/graphqlplayground/query/VehicleQuery.java
@@ -0,0 +1,26 @@
+package com.msalvatore.graphqlplayground.query;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.msalvatore.graphqlplayground.entity.Vehicle;
+import com.msalvatore.graphqlplayground.service.VehicleService;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class VehicleQuery implements GraphQLQueryResolver {
+
+    private final VehicleService vehicleService;
+
+    public VehicleQuery(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    public List<Vehicle> getVehicles(int count) {
+        return vehicleService.getAllVehicles(count);
+    }
+    public Optional<Vehicle> getVehicle(int id) {
+        return vehicleService.getVehicle(id);
+    }
+}
