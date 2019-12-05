03f5e9a93cee8ae0bf762de9d4a1916182843707
Matthew Salvatore <salvatore@railroad19.com>
2019-12-05 15:45:16 -0500

    Add new scalar types to schema and make Spring aware of them

--- src/main/java/com/msalvatore/graphqlplayground/Application.java
+++ src/main/java/com/msalvatore/graphqlplayground/Application.java
@@ -1,13 +1,20 @@
 package com.msalvatore.graphqlplayground;
 
+import com.msalvatore.graphqlplayground.scalar.datetime.GraphQLLocalDate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public GraphQLLocalDate graphQLLocalDate() {
+		return new GraphQLLocalDate();
 	}
 
 }
